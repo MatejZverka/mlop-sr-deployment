cd71f29da0f1407264e8d352ec487e2ac21667ad
Matej Zverka <matej.zverka@student.ukf.sk>
2025-04-03 23:10:45 +0200

    Add final models

---
+++ assets/asid_otf_neo_03_fp16-BRKzTPqB.js
@@ -0,0 +1,1 @@
+const o="/mlop-sr-deployment/assets/asid_otf_neo_03_fp16-Bi_HMAd2.onnx";export{o as default};
---
+++ assets/asid_otf_neo_03_fp32_optimized-Dh_0mlWQ.js
@@ -0,0 +1,1 @@
+const o="/mlop-sr-deployment/assets/asid_otf_neo_03_fp32_optimized--JluS2RY.onnx";export{o as default};
